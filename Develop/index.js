be26862dadf080225a17037a5514682f2ac6da29
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-07 01:01:05 -0500

    index

--- Develop/index.js
+++ Develop/index.js
@@ -5,14 +5,55 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of the application?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How is the application installed?',
+    },
+    {
+        type: 'input',
+        name: 'useage',
+        message: 'How is the application used?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How to contribut to this application?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How is this application tested?',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+         message: 'what is your email?',
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
